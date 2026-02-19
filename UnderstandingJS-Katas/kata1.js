7cb68178f537917e04901ab054c348f2338abd85
JC Gibbs <jgibbs@icstars.org>
2026-02-19 17:09:04 -0600

    added even number condition

--- UnderstandingJS-Katas/kata1.js
+++ UnderstandingJS-Katas/kata1.js
@@ -1,15 +1,9 @@
 // Kata 1: Print the first 10 even numbers
-// ** OBJECTIVE** Create your own loop using the following logic**
-// Commit 1: "scaffold loop structure"
-// Commit 2: "added even number condition"
-// Commit 3: "refactored variable names for clarity"
-
-
-// Kata 1: Print the first 10 even numbers
-// Commit 1: scaffold loop structure
+// Commit 2: added even number condition
 
 for (let i = 1; i <= 20; i++) {
-  console.log(i);
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
-
  
