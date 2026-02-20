99c5159765708dff9ff81fc45b9af2eb04c4aeed
JC Gibbs <jgibbs@icstars.org>
2026-02-19 19:15:03 -0600

    refactored with clear variable and comments

--- UnderstandingJS-Katas/kata2.js
+++ UnderstandingJS-Katas/kata2.js
@@ -1,14 +1,23 @@
 // Kata 2: FizzBuzz from 1 to 20
-// Commit 2: added fizzbuzz logic
+// Commit 3: refactored with clear variables and comments
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+// Loop through every number from 1 to 20
+for (let number = 1; number <= 20; number++) {
+
+  // Check divisibility by both 3 and 5 first (most specific condition)
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+
+  // Then check divisibility by 3 only
+  } else if (number % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0) {
+
+  // Then check divisibility by 5 only
+  } else if (number % 5 === 0) {
     console.log("Buzz");
+
+  // Otherwise just print the number
   } else {
-    console.log(i);
+    console.log(number);
   }
 }
