b1de70f02b33df57d51583b716bf8b66bbf41c47
JC Gibbs <jgibbs@icstars.org>
2026-02-20 08:01:25 -0600

    Scaffold nested loops

--- UnderstandingJS-Katas/kata3.js
+++ UnderstandingJS-Katas/kata3.js
@@ -1,19 +1,8 @@
 // Kata 3: Multiplication table using nested loops
-// ** OBJECTIVE** Comment out your code. To better understand nested loops**
-// Commit 1: "scaffold nested loops"
-// Commit 2: "added multiplication logic"
-// Commit 3: "refactored for readability and spacing"
+// Commit 1: scaffold nested loops
 
 for (let i = 1; i <= 5; i++) {
-  let row = "";
   for (let j = 1; j <= 5; j++) {
-    row += (i * j) + "\t";
+    // logic coming next
   }
-  console.log(row);
-}
-
-// Stretch Goal: Guard clause example
-function printNumber(num) {
-  if (num <= 0) return console.log("Please enter a positive number.");
-  console.log("Number:", num);
 }
