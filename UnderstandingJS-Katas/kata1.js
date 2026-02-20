7501fe7ff26857a8da0657fcd85b6eafae6b1775
JC Gibbs <jgibbs@icstars.org>
2026-02-19 18:39:03 -0600

    Refactorfed variable names for clarity

--- UnderstandingJS-Katas/kata1.js
+++ UnderstandingJS-Katas/kata1.js
@@ -1,8 +1,10 @@
 // Kata 1: Print the first 10 even numbers
-// Commit 2: added even number condition
+// Commit 3: refactored variable names for clarity
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
+// Start counter at 1, go up to 20
+for (let counter = 1; counter <= 20; counter++) {
+  // Check if the number is even using modulo
+  if (counter % 2 === 0) {
+    console.log(counter);
   }
 }
